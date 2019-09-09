dc3c7ae8fbedbc14be06672ba5df4bcd652728ff
BLJS2warchief <saibalaji.meruva@gmail.com>
2019-09-10 01:17:22 +0530

    The final game code

---
+++ Assets/Sprites/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Map")
+        {
+            Debug.Log("Game LoSt .... boooo");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Sprites/cameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public Transform target;
+
+    private void FixedUpdate()
+    {
+        Vector3 newPos = target.position;
+        newPos.z = -10;
+        transform.position = newPos;
+    }
+}
